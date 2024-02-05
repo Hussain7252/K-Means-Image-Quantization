cf8a5a3961cd5913cc4b9b9fc475d6689cdb0b0e
Hussain7252 <hussainkanc52@gmail.com>
2024-02-05 07:51:56 -0500

    first commit

---
+++ build/CMakeFiles/colors.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hussain/computer_vision/CourseWork/Homework4/src/colors.cpp" "CMakeFiles/colors.dir/src/colors.cpp.o" "gcc" "CMakeFiles/colors.dir/src/colors.cpp.o.d"
+  "/home/hussain/computer_vision/CourseWork/Homework4/src/kmeans.cpp" "CMakeFiles/colors.dir/src/kmeans.cpp.o" "gcc" "CMakeFiles/colors.dir/src/kmeans.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
